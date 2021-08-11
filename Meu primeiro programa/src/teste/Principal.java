dbe01ec335a9980d6fd217cb8aa42a514597b1be
Administrador <Administrador@Weslley>
2021-08-11 20:38:24 -0300

    Inclusão de comentarios para facilitar entendimento do codigo

--- Meu primeiro programa/src/teste/Principal.java
+++ Meu primeiro programa/src/teste/Principal.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("ola amigo!\nQual é o seu nome?"); // uso o println (print ln pula linhas)
-	
+		
 		Scanner in = new Scanner(System.in);
+		// Este codigo serve para cirar o objeto
 		String nome = in.nextLine();
 		System.out.printf("Olá %s\n", nome);
 		// este codigo serve para printar o nome
