7682c080d7d4b3a118953fe4a1279f0bf5619b87
schusmeicer <59938696+schusmeicer@users.noreply.github.com>
2021-08-11 20:16:26 -0300

    Update Principal.java
    comentario para definir mudanças no sistma

--- Meu primeiro programa/src/teste/Principal.java
+++ Meu primeiro programa/src/teste/Principal.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("ola amigo!\nQual È o seu nome?"); // uso o println (print ln pula linhas)
+		System.out.println("ola amigo!\nQual √© o seu nome?"); // uso o println (print ln pula linhas)
 	
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
-		System.out.printf("Ol· %s\n", nome);
+		System.out.printf("Ol√° %s\n", nome);
+		// este codigo serve para printar o nome
 	}
 
 }
