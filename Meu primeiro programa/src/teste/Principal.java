97821eba9659705efa8ee86771a461725848cd68
schusmeicer <59938696+schusmeicer@users.noreply.github.com>
2021-08-11 19:35:09 -0300

    Meu primeiro programa Java
    Primeiro programa java, feito por mim!

---
+++ Meu primeiro programa/src/teste/Principal.java
@@ -0,0 +1,15 @@
+package teste;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("ola amigo!\nQual é o seu nome?"); // uso o println (print ln pula linhas)
+	
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+		System.out.printf("Olá %s\n", nome);
+	}
+
+}
+
